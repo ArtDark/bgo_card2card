d01a766b0555bc9b7981796799e814609eb53a90
ArtDark <balusoff@gmail.com>
2020-07-05 19:57:21 +0300

    add card service

--- cmd/card2card/main.go
+++ cmd/card2card/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"github.com/ArtDark/bgo_card2card/pkg/card"
+)
+
 func main() {
+
+	svc := card.NewService("art")
+	fmt.Println(svc)
+
+	visa := svc.IssueCard("Artem", "Balusov", "Visa", "RUR")
+
+	fmt.Println(visa)
 
 }
--- pkg/card/card.go
+++ pkg/card/card.go
@@ -14,3 +14,31 @@ type Card struct {
 	Number   string
 	Icon     string
 }
+
+type Service struct {
+	BankName string
+	Cards    []*Service
+}
+
+func NewService(bankName string) *Service {
+	return &Service{BankName: bankName}
+}
+
+func (s *Service) IssueCard(fistName, lastName, issuer, currency) *Card {
+	card := &Card{
+		Id: 1 + yota,
+		Owner: Owner{
+			FirstName: fistName,
+			LastName:  lastName,
+		},
+		Issuer:   issuer,
+		Balance:  0,
+		Currency: currency,
+		Number:   "000" + string(1+yota),
+		Icon:     "https://.../logo.png",
+	}
+
+	s.Card = append(s.Cards, card)
+
+	return card
+}
