f438937f9418e710e541d51e55bf59f4d60292fa
ArtDark <balusoff@gmail.com>
2020-07-05 15:35:07 +0300

    add card struct

--- pkg/card/card.go
+++ pkg/card/card.go
@@ -1,1 +1,16 @@
 package card
+
+type Owner struct {
+	FirstName string
+	LastName  string
+}
+
+type Card struct {
+	Id int64
+	Owner
+	Issuer   string
+	Balance  int64
+	Currency string
+	Number   string
+	Icon     string
+}
