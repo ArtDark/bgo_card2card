c3b8f1a298b0d5b44e4f6bdb307f95d8b55a3f04
ArtDark <balusoff@gmail.com>
2020-07-19 22:56:01 +0300

    add new card2card method

--- pkg/transfer/fransfer.go
+++ pkg/transfer/fransfer.go
@@ -12,6 +12,27 @@ func NewService(cardSvc *card.Service, commission float64, commissionMin int64) 
 	return &Service{CardSvc: cardSvc, Commission: commission, CommissionMin: commissionMin}
 }
 
-//func (s *Service) Card2Card(from, to string, amount int) (total int, ok bool) {
-//
-//}
+func (s *Service) Card2Card(from, to string, amount int) (total int, ok bool) {
+	var fromBalance *int
+	var toBalance *int
+
+	commission := float64(amount) * s.Commission / 100.0
+
+	for _, cardNum := range s.CardSvc.Cards {
+		if from == cardNum.Number {
+			fromBalance = &cardNum.Balance
+		} else if to == cardNum.Number {
+			toBalance = &cardNum.Balance
+		} else {
+			return int(float64(amount) + commission), true
+		}
+	}
+
+	if *fromBalance >= amount {
+		*fromBalance -= int(float64(amount) + commission)
+		*toBalance += amount
+	} else {
+		return int(float64(amount) + commission), false
+	}
+	return int(float64(amount) + commission), true
+}
