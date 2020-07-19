0f607da5f4c42c1c365f3444fd41eed1232c4a9c
ArtDark <balusoff@gmail.com>
2020-07-19 23:31:23 +0300

    add "Карта чужого банка -> Карта своего банка"

--- pkg/transfer/transfer_test.go
+++ pkg/transfer/transfer_test.go
@@ -152,22 +152,106 @@ func TestService_Card2Card(t *testing.T) {
 			wantTotal: 1005_00,
 			wantOk:    true,
 		},
+		{
+			name: "Карта своего банка -> Карта чужого банка (денег недостаточно)",
+			fields: fields{
+				CardSvc: &card.Service{
+					BankName: "YourBank",
+					Cards: []*card.Card{
+						{
+							Id: "0001",
+							Owner: card.Owner{
+								FirstName: "Artem",
+								LastName:  "Balusov",
+							},
+							Issuer:   "Visa",
+							Balance:  34_34,
+							Currency: "RUR",
+							Number:   "2342_2342_5433_4322",
+							Icon:     "http://...",
+						},
+						{
+							Id: "0002",
+							Owner: card.Owner{
+								FirstName: "Ivan",
+								LastName:  "Ivanov",
+							},
+							Issuer:   "MasterCard",
+							Balance:  543534_23,
+							Currency: "RUR",
+							Number:   "4234_7563_6456_3453",
+							Icon:     "http://...",
+						},
+					},
+				},
+				Commission:    0.5,
+				CommissionMin: 10,
+			},
+			args: args{
+				from:   "2342_2342_5433_4322",
+				to:     "4234_7563_6456_3434",
+				amount: 1000_00,
+			},
+			wantTotal: 1005_00,
+			wantOk:    false,
+		},
+		{
+			name: "Карта чужого банка -> Карта своего банка",
+			fields: fields{
+				CardSvc: &card.Service{
+					BankName: "YourBank",
+					Cards: []*card.Card{
+						{
+							Id: "0001",
+							Owner: card.Owner{
+								FirstName: "Artem",
+								LastName:  "Balusov",
+							},
+							Issuer:   "Visa",
+							Balance:  43534_34,
+							Currency: "RUR",
+							Number:   "2342_2342_5433_4322",
+							Icon:     "http://...",
+						},
+						{
+							Id: "0002",
+							Owner: card.Owner{
+								FirstName: "Ivan",
+								LastName:  "Ivanov",
+							},
+							Issuer:   "MasterCard",
+							Balance:  543534_23,
+							Currency: "RUR",
+							Number:   "4234_7563_6456_3453",
+							Icon:     "http://...",
+						},
+					},
+				},
+				Commission:    0.5,
+				CommissionMin: 10,
+			},
+			args: args{
+				from:   "2342_2342_5433_4321",
+				to:     "4234_7563_6456_3453",
+				amount: 1000_00,
+			},
+			wantTotal: 1005_00,
+			wantOk:    true,
+		},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &Service{
-				CardSvc:       tt.fields.CardSvc,
-				Commission:    tt.fields.Commission,
-				CommissionMin: tt.fields.CommissionMin,
-			}
-			gotTotal, gotOk := s.Card2Card(tt.args.from, tt.args.to, tt.args.amount)
-			if gotTotal != tt.wantTotal {
-				t.Errorf("Card2Card() gotTotal = %v, want %v", gotTotal, tt.wantTotal)
-			}
-			if gotOk != tt.wantOk {
-				t.Errorf("Card2Card() gotOk = %v, want %v", gotOk, tt.wantOk)
-			}
-		})
+		s := &Service{
+			CardSvc:       tt.fields.CardSvc,
+			Commission:    tt.fields.Commission,
+			CommissionMin: tt.fields.CommissionMin,
+		}
+		gotTotal, gotOk := s.Card2Card(tt.args.from, tt.args.to, tt.args.amount)
+		if gotTotal != tt.wantTotal {
+			t.Errorf("Card2Card() gotTotal = %v, want %v", gotTotal, tt.wantTotal)
+		}
+		if gotOk != tt.wantOk {
+			t.Errorf("Card2Card() gotOk = %v, want %v", gotOk, tt.wantOk)
+		}
 	}
 }
